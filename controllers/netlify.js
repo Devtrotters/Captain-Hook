1d732dbba96103dcafb10b55f235e273821ffeef
Mathilde <mat.Grimal@gmail.com>
2021-05-12 15:29:42 +0200

    Fix : routes netlify

--- controllers/netlify.js
+++ controllers/netlify.js
@@ -1,21 +1,75 @@
 const chan = require('../channels.js');
 
 async function start(req,res) {
-  chan.netlify_channel.send("netlifyStart")
-  res.json({
-    text: "test"
-  })
-}
+  const embed = {
+    title: "STARTED",
+    color: "0xff8800",
+    fields: [
+      {
+        name: "Site name:",
+        value: req.body.name,
+      },
+      {
+        name: "URL:",
+        value: req.body.url,
+      },
+      {
+        name: "Message:",
+        value: "Build started",
+      },
+    ]
+  }
 
-async function success(req,res) {
-  chan.netlify_channel.send("netlifySuccess");
-  console.log(req);
+  chan.netlify_channel.send({ embed })
   res.send('');
 }
 
-async function fail(req,res) {
-  chan.netlify_channel.send("netlifyFail")
-  res.send('')
+async function success(req,res) {
+  const embed = {
+    title: "SUCCESS",
+    color: "0x00ff00",
+    fields: [
+      {
+        name: "Site name:",
+        value: req.body.name,
+      },
+      {
+        name: "URL:",
+        value: req.body.url,
+      },
+      {
+        name: "Message:",
+        value: "Site is live",
+      },
+    ]
+  }
+
+  chan.netlify_channel.send({ embed })
+  res.send('');
+}
+
+async function fail(req, res) {
+
+  const embed = {
+    title: "ERROR",
+    color: "0xff0000",
+    fields: [
+      {
+        name: "Site name:",
+        value: req.body.name,
+      },
+      {
+        name: "URL:",
+        value: req.body.url,
+      },
+      {
+        name: "Message:",
+        value: req.body.error_message,
+      },
+    ]
+  }
+  chan.netlify_channel.send({ embed });
+  res.send('');
 }
 
 exports.start = start;
