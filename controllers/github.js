55fd31518452518c6f2324d36b86fd34d4b63b8b
Mathilde <mat.Grimal@gmail.com>
2021-05-12 09:23:21 +0200

    Feat : route github/pull

--- controllers/github.js
+++ controllers/github.js
@@ -2,7 +2,7 @@ const chan = require('../channels.js');
 const moment = require('moment');
 moment.locale();
 
-async function discordMessage(req, res) {
+async function push(req, res) {
     const name = req.body.head_commit.author.name;
     const message = req.body.head_commit.message;
     const repo = req.body.repository.name;
@@ -15,8 +15,14 @@ async function discordMessage(req, res) {
         .addField("Date : ", date.moment().format('Do MMMM YYYY, h:mm:ss a'));
         chan.github_channel.send(embed);
         res.send('');
-
-    
 }
 
-exports.discordMessage = discordMessage;
+exports.push = push;
+
+
+async function pullRequest(req, res) {
+    console.log(req);
+    chan.github_channel.send('Pull Request');
+}
+
+exports.pullRequest = pullRequest;
