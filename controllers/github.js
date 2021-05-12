408e604ce54d2dde1c10246951b5161d177e7099
Mathilde <mat.Grimal@gmail.com>
2021-05-12 10:44:41 +0200

    fix push request

--- controllers/github.js
+++ controllers/github.js
@@ -2,17 +2,17 @@ const chan = require('../channels.js');
 
 async function push(req, res) {
     console.log(req);
-    // const name = req.body.head_commit.author.name;
-    // const message = req.body.head_commit.message;
-    // const repo = req.body.repository.name;
-    // const date = req.body.head_commit.timestamp;
-    // const embed = new MessageEmbed()
-    //     .setColor("GREEN")
-    //     .addField("Repo : ", repo)
-    //     .addField("Auteur du push : ", name)
-    //     .addField("Message du commit : ", message || "Aucun contenu")
-    //     .addField("Date : ", date);
-    //     chan.github_channel.send(embed);
+    const name = req.body.pusher.name;
+    const message = req.body.head_commit.message;
+    const repo = req.body.repository.name;
+    const date = req.body.head_commit.timestamp;
+    const embed = new MessageEmbed()
+        .setColor("GREEN")
+        .addField("Repo : ", repo)
+        .addField("Auteur du push : ", name)
+        .addField("Message du commit : ", message || "Aucun contenu")
+        .addField("Date : ", date);
+        chan.github_channel.send(embed);
         res.json({
             text: "test"
           })
