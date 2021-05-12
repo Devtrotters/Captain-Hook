6e0282d9e07c3ca3dafa0faf83e29ee510a0141f
Mathilde <mat.Grimal@gmail.com>
2021-05-12 13:30:32 +0200

    Fix route github/pull

--- controllers/github.js
+++ controllers/github.js
@@ -36,8 +36,8 @@ async function pullRequest(req, res) {
     const name = req.body.sender.login;
     const repo = req.body.repository.name;
     const date = req.body.pull_request.created_at;
-    const brancheHead = req.body.head.ref;
-    const brancheBase = req.body.base.ref;
+    const brancheHead = req.body.pull_request.head.ref;
+    const brancheBase = req.body.pull_request.base.ref;
     const message = 'branche '+brancheHead+' vers branche '+brancheBase;
     const embedMessage = new MessageEmbed()
         .setColor("ORANGE")
