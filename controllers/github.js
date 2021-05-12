40cabe72ea3e15a892cdde90e2ec5823b48a68ae
Mathilde <mat.Grimal@gmail.com>
2021-05-12 15:19:30 +0200

    Other changes to notifications

--- controllers/github.js
+++ controllers/github.js
@@ -51,7 +51,7 @@ async function pullRequest(req, res) {
     const brancheBase = req.body.pull_request.base.ref;
     const branches = 'branche '+brancheHead+' vers branche '+brancheBase;
     console.log(state);
-    if(state == 'opened') {
+    if(state == 'open') {
         console.log('#### pull opened notification request ###');
         const embedMessage = new MessageEmbed()
             .setColor("ORANGE")
